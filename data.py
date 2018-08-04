fece44c0b832c13b4a1e5fa3db76210767e1fe22
rbilkevic-kpmg <robert.bilkevic@kpmg.co.uk>
2018-08-04 20:55:45 +0100

    Data split

--- data.py
+++ data.py
@@ -23,3 +23,27 @@ class ElisaData:
 
         self._wb.save(self.elisa_data.replace('input\\', 'output\\').replace(".xlsx", "_result.xlsx"))
         print("Output saved in output folder")
+
+
+class CRPData:
+    def __init__(self, elisa_data):
+        self.elisa_data = elisa_data
+        self._process()
+
+    def _process(self):
+        self._wb = openpyxl.load_workbook(filename=self.elisa_data, data_only=True)
+        self._sheet = self._wb["End point_1"]
+
+        tmp = [self._sheet.cell(row=row, column=col).value for col in range(2, 14) for row in range(31, 35)]
+
+        self.STANDARDS_DATA = [tmp[0]] + tmp[2:8]
+        self.DATA = tmp[8:]
+
+    def write_concentrations(self, conc_data):
+        conc_data = 8 * [None] + list(conc_data)
+        for col in range(2, 14):
+            for row in range(38, 42):
+                self._sheet.cell(row=row, column=col).value = conc_data.pop(0)
+
+        self._wb.save(self.elisa_data.replace('input\\', 'output\\').replace(".xlsx", "_result.xlsx"))
+        print("Output saved in output folder")
