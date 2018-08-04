109cb2097582c86df92f6e18239f5a96d5aa26cd
rbilkevic-kpmg <robert.bilkevic@kpmg.co.uk>
2018-08-04 22:02:21 +0100

    more robust process

--- data.py
+++ data.py
@@ -7,21 +7,24 @@ class ElisaData:
         self._process()
 
     def _process(self):
-        self._wb = openpyxl.load_workbook(filename=self.elisa_data, data_only=True)
-        self._sheet = self._wb["End point_1"]
+        wb = openpyxl.load_workbook(filename=self.elisa_data, data_only=True)
+        sheet = wb["End point_1"]
 
-        tmp = [self._sheet.cell(row=row, column=col).value for col in range(2, 14) for row in range(31, 35)]
+        tmp = [sheet.cell(row=row, column=col).value for col in range(2, 14) for row in range(31, 35)]
 
         self.STANDARDS_DATA = tmp[1:8]
         self.DATA = tmp[9:]
 
     def write_concentrations(self, conc_data):
+        wb = openpyxl.load_workbook(filename=self.elisa_data)
+        sheet = wb["End point_1"]
+
         conc_data = 9 * [None] + list(conc_data)
         for col in range(2, 14):
             for row in range(38, 42):
-                self._sheet.cell(row=row, column=col).value = conc_data.pop(0)
+                sheet.cell(row=row, column=col).value = conc_data.pop(0)
 
-        self._wb.save(self.elisa_data.replace('input\\', 'output\\').replace(".xlsx", "_result.xlsx"))
+        wb.save(self.elisa_data.replace('input\\', 'output\\').replace(".xlsx", "_result.xlsx"))
         print("Output saved in output folder")
 
 
@@ -31,19 +34,22 @@ class CRPData:
         self._process()
 
     def _process(self):
-        self._wb = openpyxl.load_workbook(filename=self.elisa_data, data_only=True)
-        self._sheet = self._wb["End point_1"]
+        wb = openpyxl.load_workbook(filename=self.elisa_data, data_only=True)
+        sheet = wb["End point_1"]
 
-        tmp = [self._sheet.cell(row=row, column=col).value for col in range(2, 14) for row in range(24, 28)]
+        tmp = [sheet.cell(row=row, column=col).value for col in range(2, 14) for row in range(24, 28)]
 
         self.STANDARDS_DATA = [tmp[0]] + tmp[2:8]
         self.DATA = tmp[8:]
 
     def write_concentrations(self, conc_data):
+        wb = openpyxl.load_workbook(filename=self.elisa_data)
+        sheet = wb["End point_1"]
+
         conc_data = 8 * [None] + list(conc_data)
         for col in range(2, 14):
             for row in range(31, 35):
-                self._sheet.cell(row=row, column=col).value = conc_data.pop(0)
+                sheet.cell(row=row, column=col).value = conc_data.pop(0)
 
-        self._wb.save(self.elisa_data.replace('input\\', 'output\\').replace(".xlsx", "_result.xlsx"))
+        wb.save(self.elisa_data.replace('input\\', 'output\\').replace(".xlsx", "_result.xlsx"))
         print("Output saved in output folder")
