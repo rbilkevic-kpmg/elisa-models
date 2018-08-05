0ff99acc1c09678336b2b4eaef9e8214ec87b3c2
rbilkevic-kpmg <robert.bilkevic@kpmg.co.uk>
2018-08-05 16:41:26 +0100

    graphical analysis 2

--- analysis.py
+++ analysis.py
@@ -1,181 +1,69 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
+import re
 from scipy.stats import pearsonr
 
 
+def draw_boxplot(col, categories, dpi=200):
+    data.boxplot(column=col, by=categories)
+    pattern = re.compile('[\W_]+')
+    col = pattern.sub('', col)
+    categories = pattern.sub('', categories)
 
-data = pd.read_excel('BMA analysis.xlsx', index_col=0, na_values=np.float64('nan'))
+    plt.grid(False)
+    plt.savefig('graphs\\boxplot_{}_{}'.format(col.lower(), categories.lower()), dpi=dpi)
+
+
+def draw_scatter(x_n, y_n, dpi=200):
+    data.plot(x=x_n, y=y_n, kind='scatter')
+    corr = pearsonr(data[x_n], data[y_n])
+    x_pos = min(data[x_n])  # + (max(data[x_n]) - min(data[x_n])) * 0.05
+    y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
+    plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
+
+    pattern = re.compile('[\W_]+')
+    x_n = pattern.sub('', x_n)
+    y_n = pattern.sub('', y_n)
+    plt.savefig('graphs\\boxplot_{}_{}'.format(x_n.lower(), y_n.lower()), dpi=dpi)
+
+
+data = pd.read_excel('BMA analysis.xlsx', index_col=0)
 
 data['Is_Healthy'] = [1 if row['Category'] == 'Healthy' else 0 for ix, row in data.iterrows()]
 data['Is_Obese'] = [1 if row['Category'] == 'Obese' else 0 for ix, row in data.iterrows()]
 data['Is_Severe'] = [1 if row['Category'] == 'Severe Obesity' else 0 for ix, row in data.iterrows()]
 data['Is_Morbid'] = [1 if row['Category'] == 'Morbidly Obese' else 0 for ix, row in data.iterrows()]
-
-
 data['Female'] = [1 if row['Gender'] == 'F' else 0 for ix, row in data.iterrows()]
+
 data = data.dropna(axis=0)
 
-#print(data)
-
 # Primary analysis
-dpi = 200
+draw_boxplot(col='Age', categories='Gender')
+draw_boxplot(col='Body Fat, %', categories='Gender')
+draw_boxplot(col='Body Fat, %', categories='Is_Healthy')
+draw_boxplot(col='BMI', categories='Is_Healthy')
+draw_boxplot(col='WHR', categories='Is_Healthy')
+draw_boxplot(col='WHR', categories='Is_Healthy')
+draw_boxplot(col='8OHDG', categories='Is_Healthy')
+draw_boxplot(col='8OHDG', categories='Gender')
+draw_boxplot(col='CRP', categories='Is_Healthy')
+draw_boxplot(col='CRP', categories='Gender')
+draw_boxplot(col='DNA DAMAGE, %', categories='Is_Healthy')
+draw_boxplot(col='DNA DAMAGE, %', categories='Gender')
 
-data.boxplot(column='Age', by='Gender')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_age_gender', bbox_inches='tight', dpi=dpi)
-
-data.boxplot(column='Body Fat, %', by='Gender')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_fat_gender', bbox_inches='tight', dpi=dpi)
-
-data.boxplot(column='Body Fat, %', by='Is_Healthy')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_fat_healthy', bbox_inches='tight', dpi=dpi)
-
-data.boxplot(column='BMI', by='Is_Healthy')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_bmi_healthy', bbox_inches='tight', dpi=dpi)
-
-data.boxplot(column='WHR', by='Is_Healthy')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_whr_healthy', bbox_inches='tight', dpi=dpi)
-
-data.boxplot(column='8OHDG', by='Is_Healthy')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_8ohdg_healthy', bbox_inches='tight', dpi=dpi)
-
-data.boxplot(column='8OHDG', by='Gender')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_8ohdg_gender', bbox_inches='tight', dpi=dpi)
-
-data.boxplot(column='CRP', by='Is_Healthy')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_crp_healthy', bbox_inches='tight', dpi=dpi)
-
-data.boxplot(column='CRP', by='Gender')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_crp_gender', bbox_inches='tight', dpi=dpi)
-
-data.boxplot(column='DNA DAMAGE, %', by='Is_Healthy')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_dnadmg_healthy', bbox_inches='tight', dpi=dpi)
-
-data.boxplot(column='DNA DAMAGE, %', by='Gender')
-plt.grid(False)
-plt.savefig('graphs\\boxplot_dnadmg_gender', bbox_inches='tight', dpi=dpi)
-
-x_n = 'DNA DAMAGE, %'
-y_n = '8OHDG'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_dnadmg_8ohdg', bbox_inches='tight', dpi=dpi)
-
-x_n = 'CRP'
-y_n = '8OHDG'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_crp_8ohdg', bbox_inches='tight', dpi=dpi)
-
-x_n = 'BMI'
-y_n = '8OHDG'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_bmi_8ohdg', bbox_inches='tight', dpi=dpi)
-
-x_n = 'Body Fat, %'
-y_n = '8OHDG'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_fat_8ohdg', bbox_inches='tight', dpi=dpi)
-
-x_n = 'WHR'
-y_n = '8OHDG'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_whr_8ohdg', bbox_inches='tight', dpi=dpi)
-
-
-x_n = 'DNA DAMAGE, %'
-y_n = 'CRP'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_dnadmg_crp', bbox_inches='tight', dpi=dpi)
-
-x_n = 'BMI'
-y_n = 'CRP'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_bmi_crp', bbox_inches='tight', dpi=dpi)
-
-x_n = 'Body Fat, %'
-y_n = 'CRP'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_fat_crp', bbox_inches='tight', dpi=dpi)
-
-x_n = 'WHR'
-y_n = 'CRP'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_whr_crp', bbox_inches='tight', dpi=dpi)
-
-
-
-x_n = 'BMI'
-y_n = 'DNA DAMAGE, %'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_bmi_dnadmg', bbox_inches='tight', dpi=dpi)
-
-x_n = 'Body Fat, %'
-y_n = 'DNA DAMAGE, %'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_fat_dnadmg', bbox_inches='tight', dpi=dpi)
-
-x_n = 'WHR'
-y_n = 'DNA DAMAGE, %'
-data.plot(x=x_n, y=y_n, kind='scatter')
-corr = pearsonr(data[x_n], data[y_n])
-x_pos = min(data[x_n]) #+ (max(data[x_n]) - min(data[x_n])) * 0.05
-y_pos = max(data[y_n]) - (max(data[y_n]) - min(data[y_n])) * 0.05
-plt.text(x_pos, y_pos, r"$\rho_{xy}=%0.2f$" % corr[0] + "\n" + r"${p-value}=%0.3f$" % corr[1])
-plt.savefig('graphs\\scatter_whr_dnadmg', bbox_inches='tight', dpi=dpi)
+draw_scatter(x_n='DNA DAMAGE, %', y_n='8OHDG')
+draw_scatter(x_n='CRP', y_n='8OHDG')
+draw_scatter(x_n='BMI', y_n='8OHDG')
+draw_scatter(x_n='Body Fat, %', y_n='8OHDG')
+draw_scatter(x_n='WHR', y_n='8OHDG')
+draw_scatter(x_n='DNA DAMAGE, %', y_n='CRP')
+draw_scatter(x_n='BMI', y_n='CRP')
+draw_scatter(x_n='Body Fat, %', y_n='CRP')
+draw_scatter(x_n='WHR', y_n='CRP')
+draw_scatter(x_n='BMI', y_n='DNA DAMAGE, %')
+draw_scatter(x_n='Body Fat, %', y_n='DNA DAMAGE, %')
+draw_scatter(x_n='WHR', y_n='DNA DAMAGE, %')
 
 #
 # X = data[["Is_Healthy", "Female", "DNA DAMAGE, %", "CRP", "WHR", "Age", "BMI"]]
